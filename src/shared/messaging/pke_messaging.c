d08738dfe51b483b6f35ce53fff07df323788678
Welby Seely <welbyseely@gmail.com>
2025-10-20 20:22:34 -0400

    Symmetric serialize/deserialize

--- src/shared/messaging/pke_messaging.c
+++ src/shared/messaging/pke_messaging.c
@@ -18,6 +18,34 @@ int serializePKClientRequest(PClientToPKServer toSerialize, char *serialized) {
   return 0;
 }
 
+int deserializePKClientRequest(char *serialized, PClientToPKServer *deserialized) {
+
+  int messageType;
+  int userID;
+  int publicKey;
+
+  memcpy(&messageType, serialized, sizeof(int));
+  memcpy(&userID, serialized + sizeof(int), sizeof(int));
+  memcpy(&publicKey, serialized + sizeof(int) * 2, sizeof(int));
+  deserialized->messageType = messageType;
+  deserialized->userID = userID;
+  deserialized->publicKey= publicKey;
+  return 0;
+}
+
+int serializePKServerResponse(PKServerToLodiClient toSerialize, char *serialized) {
+  // int messageType = htonl(toSerialize.messageType);
+  // int userID = htonl(toSerialize.userID);
+  int messageType = toSerialize.messageType;
+  int userID = toSerialize.userID;
+  int publicKey = toSerialize.publicKey;
+  memset(serialized, 0, 12);
+  memcpy(serialized, &messageType, sizeof(messageType));
+  memcpy(serialized + sizeof(messageType), &userID, sizeof(userID));
+  memcpy(serialized + sizeof(messageType) + sizeof(userID), &publicKey, sizeof(publicKey));
+  return 0;
+}
+
 int deserializePKServerResponse(char *serialized, PKServerToLodiClient *deserialized) {
 
   int messageType;
