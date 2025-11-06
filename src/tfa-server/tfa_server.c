51da0a9be5a34a1c6bb9cf63938cb19ed70a4194
Welby Seely <welbyseely@gmail.com>
2025-11-05 20:06:21 -0500

    Authenticate on TFA Server

--- src/tfa-server/tfa_server.c
+++ src/tfa-server/tfa_server.c
@@ -6,12 +6,17 @@
 #include "shared.h"
 #include "registration_repository.h"
 #include "messaging/tfa_messaging.h"
+#include "messaging/pke_messaging.h"
 #include "messaging/udp.h"
+#include "util/rsa.h"
 #include "util/server_configs.h"
 
 int main() {
     init();
     const unsigned short serverPort = atoi(getServerConfig(TFA).port);
+    const ServerConfig pkServerConfig = getServerConfig(PK);
+    const unsigned int pkPort = atoi(pkServerConfig.port);
+    const char * pkAddress = pkServerConfig.address;
 
     const int serverSocket = getServerSocket(serverPort, NULL);
     if (serverSocket < 0) {
@@ -34,18 +39,50 @@ int main() {
         TFAClientOrLodiServerToTFAServer *receivedMessage = deserializeTFAClientRequest(
             receivedBuffer, TFA_CLIENT_REQUEST_SIZE);
 
+        // GET PUBLIC KEY
+        PClientToPKServer pkRequest = {
+            .messageType= requestKey,
+            .userID = receivedMessage->userID
+        };
 
-        TFAServerToTFAClient toSendMessage = {
+        char *pkRequestBuffer = serializePKClientRequest(&pkRequest);
+        char pkResponseBuffer[PK_SERVER_RESPONSE_SIZE];
+        const int pkSendStatus = synchronousSend(pkRequestBuffer, PK_CLIENT_REQUEST_SIZE, pkResponseBuffer,
+                                           PK_SERVER_RESPONSE_SIZE, pkAddress, pkPort);
+        free(pkRequestBuffer);
+
+        if (pkSendStatus == ERROR) {
+            printf("Error while sending PK public key request message. Continuing...\n");
+            continue;
+        }
+
+        PKServerToLodiClient *pkResponseDeserialized = deserializePKServerResponse(
+            pkResponseBuffer, PK_SERVER_RESPONSE_SIZE);
+        printf("Got public key! Received: messageType=%u, userID=%u, publicKey=%u\n",
+               pkResponseDeserialized->messageType, pkResponseDeserialized->userID, pkResponseDeserialized->publicKey);
+        const unsigned long publicKey = pkResponseDeserialized->publicKey;
+        free(pkResponseDeserialized);
+
+        // GOT PUBLIC KEY, NOW AUTHENTICATE
+        const unsigned long digitalSig = receivedMessage->digitalSig;
+        const unsigned long timestamp = receivedMessage->timestamp;
+        const unsigned long decryptedTimesstamp = decryptTimestamp(digitalSig, publicKey, MODULUS);
+        if (decryptedTimesstamp != timestamp) {
+            printf("Authentication failed! Aborting TFA client registration...\n");
+            continue;
+        }
+        printf("Authentication succeeded! Continuing with TFA client registration!\n");
+
+        // REGISTER CLIENT
+        addIP(receivedMessage->userID, clientAddress.sin_addr, ntohs(clientAddress.sin_port));
+        printf("Registered client! Sending TFA confirmation message!\n");
+
+        // WE AUTHENTICATED! SEND SUCCESS MESSAGE TO CLIENT
+        TFAServerToTFAClient registrationSuccessMessage = {
             confirmTFA,
             receivedMessage->userID,
         };
-
-        addIP(receivedMessage->userID, clientAddress.sin_addr, ntohs(clientAddress.sin_port));
-        struct in_addr testRetrieve;
-        unsigned short testPort;
-        getIP(receivedMessage->userID, &testRetrieve, &testPort);
-
-        char *sendBuffer = serializeTFAServerResponse(&toSendMessage);
+        char *sendBuffer = serializeTFAServerResponse(&registrationSuccessMessage);
 
         const int sendSuccess = sendMessage(serverSocket, sendBuffer, TFA_SERVER_RESPONSE_SIZE, &clientAddress);
         if (sendSuccess == ERROR) {
