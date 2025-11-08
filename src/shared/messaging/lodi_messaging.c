e0f0f6ed27a131772803d0dcfe5c62340c52049f
Welby Seely <welbyseely@gmail.com>
2025-11-07 22:08:51 -0500

    Remove cruft

--- src/shared/messaging/lodi_messaging.c
+++ src/shared/messaging/lodi_messaging.c
@@ -14,55 +14,6 @@
 #include "util/buffers.h"
 #include "util/server_configs.h"
 
-
-char *serializeLodiServerRequest(const PClientToLodiServer *toSerialize) {
-  char *serialized = malloc(LODI_CLIENT_REQUEST_SIZE);
-  if (!serialized) {
-    return NULL;
-  }
-
-  size_t offset = 0;
-  appendUint32(serialized, &offset, toSerialize->messageType);
-  appendUint32(serialized, &offset, toSerialize->userID);
-  appendUint32(serialized, &offset, toSerialize->recipientID);
-  appendUint64(serialized, &offset, toSerialize->timestamp);
-  appendUint64(serialized, &offset, toSerialize->digitalSig);
-  return serialized;
-}
-
-PClientToLodiServer *deserializeLodiServerRequest(const char *serialized, const size_t size) {
-  // validate buffer/serialized size
-  if (size < LODI_CLIENT_REQUEST_SIZE) {
-    return NULL;
-  }
-
-  PClientToLodiServer *deserialized = malloc(sizeof(*deserialized));
-  if (!deserialized) {
-    return NULL;
-  }
-
-  size_t offset = 0;
-  deserialized->messageType = getUint32(serialized, &offset);
-  deserialized->userID = getUint32(serialized, &offset);
-  deserialized->recipientID = getUint32(serialized, &offset);
-  deserialized->timestamp = getUint64(serialized, &offset);
-  deserialized->digitalSig = getUint64(serialized, &offset);
-
-  return deserialized;
-}
-
-char *serializeLodiServerResponse(const LodiServerToLodiClientAcks *toSerialize) {
-  char *serialized = malloc(LODI_SERVER_RESPONSE_SIZE);
-  if (!serialized) {
-    return NULL;
-  }
-
-  size_t offset = 0;
-  appendUint32(serialized, &offset, toSerialize->messageType);
-  appendUint32(serialized, &offset, toSerialize->userID);
-  return serialized;
-}
-
 LodiServerToLodiClientAcks *deserializeLodiServerResponse(const char *serialized, const size_t size) {
   // validate buffer/serialized size
   if (size < LODI_SERVER_RESPONSE_SIZE) {
